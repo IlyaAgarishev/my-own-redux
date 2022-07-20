142341d77e15032bcb93d573c12f6746c3e4f543
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-20 20:10:34 +0500

    dispatched new action "ADD_HOBBY"

--- src/components/HobbiesForm/HobbiesForm.tsx
+++ src/components/HobbiesForm/HobbiesForm.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Store from "../../myOwnRedux/store";
 
 const HobbiesForm = () => {
+  const [value, setValue] = useState("");
+
+  const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const addHobby = () => {
+    Store.dispatch({ type: "ADD_HOBBY", payload: value });
+  };
+
   return (
     <span>
-      <input type="text" placeholder="Hobby" />
-      <button>Add</button>
+      <input type="text" placeholder="Hobby" onChange={onValueChange} />
+      <button onClick={addHobby}>Add</button>
     </span>
   );
 };
