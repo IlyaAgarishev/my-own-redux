283f11a17f8b1e33d680e577e14bf4675256690e
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-21 21:08:02 +0500

    Titles updated

--- src/components/Titles/Titles.tsx
+++ src/components/Titles/Titles.tsx
@@ -1,13 +1,14 @@
 const Titles = () => {
   return (
     <>
-      <h1>Testing how my-own-redux works</h1>
-      <h4>
+      <h1>my-own-redux</h1>
+      <h3>
         Check console to see redux state,{" "}
         <span style={{ color: "green" }}>dispatch</span> and{" "}
         <span style={{ color: "red" }}>re</span>-
         <span style={{ color: "blue" }}>render</span> logger
-      </h4>
+      </h3>
+      <h4>Then refresh the page to see how persist works</h4>
     </>
   );
 };
