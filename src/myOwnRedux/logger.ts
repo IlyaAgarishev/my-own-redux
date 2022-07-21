eb9e63bff7087b5e9fbbf0a2cda274030830c48f
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-21 21:13:50 +0500

    toUpperCase removed

--- src/myOwnRedux/logger.ts
+++ src/myOwnRedux/logger.ts
@@ -2,7 +2,7 @@ import { Action, State } from "./types";
 
 export default (prevState: State, newState: State, action: Action) => {
   console.log(
-    `%cdipatched %c${JSON.stringify(action).toUpperCase()}`,
+    `%cdipatched %c${JSON.stringify(action)}`,
     "color:green; font-weight:bold;",
     "color:green; font-weight:bold;"
   );
